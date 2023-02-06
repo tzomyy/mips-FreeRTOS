d94f1c8f6a90970e546870a9f2afeb18c8d8eafc
tzomyy <106305602+tzomyy@users.noreply.github.com>
2023-02-06 21:36:03 +0100

    Made 2 tasks of same function with same priority

--- Core/Src/example.c
+++ Core/Src/example.c
@@ -9,16 +9,18 @@
 #include "example.h"
 #include "gpio.h"
 
-void exampleTaskFunc(void *function) {
+void exampleTaskFunc(void *param) {
 
-	while(1){
-		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
+	while (1) {
+		HAL_GPIO_TogglePin(GPIOC, (uint32_t) param);
 		vTaskDelay(1000);
 	}
 }
 
-
 void exampleInit() {
-	xTaskCreate(exampleTaskFunc, "exampleTaskFunc", 128, NULL, 1, NULL);
+	xTaskCreate(exampleTaskFunc, "exampleTaskFunc0", 128, (void*) GPIO_PIN_0, 1,
+			NULL);
+	xTaskCreate(exampleTaskFunc, "exampleTaskFunc1", 128, (void*) GPIO_PIN_1, 1,
+			NULL);
 }
 

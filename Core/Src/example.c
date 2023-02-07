572846177a4fe09967ee1431920e65a36554ac92
tzomyy <106305602+tzomyy@users.noreply.github.com>
2023-02-08 00:07:50 +0100

    Deffered Interrupt Handling with Deamon task

--- Core/Src/example.c
+++ Core/Src/example.c
@@ -4,30 +4,30 @@
  *  Created on: Feb 6, 2023
  *      Author: Laptop
  */
+
 #include "FreeRTOS.h"
 #include "task.h"
+#include "timers.h"
 #include "example.h"
 #include "gpio.h"
 
 TaskHandle_t controllerHandle, defferedHandle;
 
-void defferedTask(void *param) {
-	while (1) {
-		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
-	}
-}
+void callBackDeffered(void *p1, uint32_t p2);
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 	if (GPIO_Pin == GPIO_PIN_0) {
 		BaseType_t woken = pdFALSE;
-		vTaskNotifyGiveFromISR(defferedHandle, &woken);
+		xTimerPendFunctionCallFromISR(callBackDeffered, NULL, 0, &woken);
 		portYIELD_FROM_ISR(woken);
 	}
 }
 
 void exampleInit() {
-	xTaskCreate(defferedTask, "defferedTask", 128, (void*) GPIO_PIN_0, 2,
-			&defferedHandle);
+
+}
+
+void callBackDeffered(void *p1, uint32_t p2) {
+	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
 }
 

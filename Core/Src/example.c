06fc0d18d68e54ce54e62650d8c72564622add87
tzomyy <106305602+tzomyy@users.noreply.github.com>
2023-02-08 15:05:04 +0100

    Toggling with queue

--- Core/Src/example.c
+++ Core/Src/example.c
@@ -8,26 +8,37 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "timers.h"
+#include "queue.h"
 #include "example.h"
 #include "gpio.h"
 
 TaskHandle_t controllerHandle, defferedHandle;
+QueueHandle_t queueHandle;
 
 void callBackDeffered(void *p1, uint32_t p2);
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-	if (GPIO_Pin == GPIO_PIN_0) {
-		BaseType_t woken = pdFALSE;
-		xTimerPendFunctionCallFromISR(callBackDeffered, NULL, 0, &woken);
-		portYIELD_FROM_ISR(woken);
+
+	uint32_t itemToQueue = GPIO_Pin;
+	BaseType_t woken = pdFALSE;
+	xQueueSendToBackFromISR(queueHandle, &itemToQueue, &woken);
+	portYIELD_FROM_ISR(woken);
+
+}
+
+void taskToggler(void *param) {
+	uint32_t bufferForPinIndex;
+
+	while (1) {
+		xQueueReceive(queueHandle, &bufferForPinIndex, portMAX_DELAY);
+		HAL_GPIO_TogglePin(GPIOC, bufferForPinIndex);
 	}
+
 }
 
 void exampleInit() {
+	queueHandle = xQueueCreate(10, sizeof(uint32_t));
+	xTaskCreate(taskToggler, "taskToggler", 128, NULL, 1, NULL);
 
-}
-
-void callBackDeffered(void *p1, uint32_t p2) {
-	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
 }
 
